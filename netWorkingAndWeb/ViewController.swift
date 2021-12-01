010381aee8a3acc021d36ff9511a0283206cac58
bilal-sagir <sagir.bilal@icloud.com>
2021-12-01 18:55:18 +0300

    convert JsonString

--- netWorkingAndWeb/ViewController.swift
+++ netWorkingAndWeb/ViewController.swift
@@ -7,7 +7,28 @@ class ViewController: UIViewController
     
     override func viewDidLoad()
     {
-        super.viewDidLoad()
+        json()
+        //super.viewDidLoad()
+        
     }
 }
 
+struct Property : Codable{
+    var price: Int
+    var id: String
+    var type: String
+    var img_src: String
+    
+}
+
+func json(){
+    
+    let JsonString = """
+{"price":450000,"id":"424905","type":"buy","img_src":"http://mars.jpl.nasa.gov/msl-raw-images/msss/01000/mcam/1000MR0044631300503690E01_DXXX.jpg"}
+"""
+    if let jsondata = JsonString.data(using: .utf8){
+        let decoder = JSONDecoder()
+        let prop = try! decoder.decode(Property.self, from: jsondata)
+        print(prop.price)
+    }
+}
