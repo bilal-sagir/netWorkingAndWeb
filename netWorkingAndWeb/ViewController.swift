f8659049716060a0e93836ee92483abc0d54ec32
bilal-sagir <sagir.bilal@icloud.com>
2021-12-01 03:26:58 +0300

    testproject for webview

--- netWorkingAndWeb/ViewController.swift
+++ netWorkingAndWeb/ViewController.swift
@@ -1,13 +1,26 @@
 import UIKit
+import WebKit
 
 class ViewController: UIViewController
 {
+    @IBOutlet weak var myWeb : WKWebView!
+    
     
     //check branches of the project
     
     override func viewDidLoad()
     {
         super.viewDidLoad()
+        
+        myWeb.allowsBackForwardNavigationGestures = true
+        
+        guard let url = URL(string: "https://www.hackingwithswift.com") else {return}
+        
+        let request = URLRequest(url: url)
+        
+        myWeb.load(request)
+        
+        
     }
 }
 
