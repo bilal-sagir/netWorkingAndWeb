efc4c6e10a93f2cc4753ddb58e0e0e0dfc76e89d
bilal-sagir <sagir.bilal@icloud.com>
2021-12-01 18:22:31 +0300

    use safari services

--- netWorkingAndWeb/ViewController.swift
+++ netWorkingAndWeb/ViewController.swift
@@ -1,13 +1,26 @@
 import UIKit
+import SafariServices
+
 
 class ViewController: UIViewController
 {
-    
-    //check branches of the project
-    
-    override func viewDidLoad()
+    @IBAction func loadWithinApp()
     {
-        super.viewDidLoad()
+        guard let url = URL(string: "https://www.hackingwithswift.com") else {return}
+        
+        let sfVc = SFSafariViewController(url: url)
+        
+        self.present(sfVc, animated: true, completion: nil)
+        
     }
+    
+    
+    @IBAction func loadExternally()
+    {
+        guard let url = URL(string: "https://www.hackingwithswift.com") else {return}
+        UIApplication.shared.open(url, options: [:], completionHandler: nil)
+        
+    }
+    
 }
 
